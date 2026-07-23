639a872e89d27cca4e146a82fb0912f285b85252
2dollar2dollar <vladislav.grechushkin.666@gmail.com>
2026-07-24 01:16:00 +0300

    feat: some fixies

--- src/store/reducers/profile.reducer.ts
+++ src/store/reducers/profile.reducer.ts
@@ -3,6 +3,7 @@ import {
   ProfileActionUnion,
   fetchProfileAction,
   updateProfileAction,
+  uploadAvatarAction,
 } from '../actions/profile.action';
 import { AuthSuccessPayload } from '../actions/auth.action';
 
@@ -48,4 +49,8 @@ export const profileReducer = createReducer<ProfileState, ProfileActionUnion>(in
     ...state,
     isUpdating: false,
     error: action.payload,
+  }))
+  .handleAction(uploadAvatarAction.success, (state, action) => ({ // 👈 2. Добавь обработку загрузки аватарки!
+    ...state,
+    data: state.data ? { ...state.data, profile_image_url: action.payload } : null,
   }));
